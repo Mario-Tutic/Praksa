c121afbbbb9a048fd5adc691bb812f8317574baf
DESKTOP-KTD1H84\mario <mario.tutic00@gmail.com>
2022-02-02 09:44:27 +0100

    day 7

---
+++ Project1WebApiDay5/Project1WebApiDay2/ContainerConfig.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using University.Model;
+using University.Model.Common;
+using Student.Service.Common;
+using Student.Service;
+using Student.Repository.Common;
+using Student.Repository;
+namespace Project1WebApiDay2
+{
+    public class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Course>().As<ICourse>();
+            builder.RegisterType<StudentInfo>().As<IStudentInfo>();
+            builder.RegisterType<CourseService>().As<ICourseService>();
+            builder.RegisterType<StudentService>().As<IStudentService>();
+            builder.RegisterType<StudentRepository>().As<IStudentRepository>();
+            builder.RegisterType<CourseRepository>().As<ICourseRepository>();
+            return builder.Build();
+
+        }
+    }
+}
